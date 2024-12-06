77a97cd51789344de7f39eafdaa4b8f83c9d1165
Elkherchy <elkherchymd22025@gmail.com>
2024-12-06 04:39:36 +0000

    first commit

--- meilisearch-site/src/pages/Home.jsx
+++ meilisearch-site/src/pages/Home.jsx
@@ -6,10 +6,12 @@ const Home = () => {
   const [results, setResults] = useState([]);
 
   return (
-    <div className="p-4">
-      <h1 className="text-center text-4xl mb-4">Search Quest</h1>
-      <SearchBar setResults={setResults} />
-      <SearchResults results={results} />
+    <div className="flex justify-center h-screen pt-10 bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl mb-4">Search Quest</h1>
+        <SearchBar setResults={setResults} />
+        <SearchResults results={results} />
+      </div>
     </div>
   );
 };
